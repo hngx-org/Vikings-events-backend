23fb6b488a59b31f8fdd8c6e9586588e7d2e686a
Oluwakayode Adesina <adesinak4@gmail.com>
2023-09-21 19:41:42 +0100

    Update passport.js

--- src/utils/passport.js
+++ src/utils/passport.js
@@ -12,7 +12,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3306/api/users/auth/google/callback',
+        callbackURL: 'https://vikings-events-backend-production-2596.up.railway.app/api/users/auth/google/callback',
         passReqToCallback: true,
       },
       async (request, accessToken, refreshToken, profile, done) => {
