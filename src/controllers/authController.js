0d25a9feeb6cef15f56d984fc9f97367fc07fa60
Hack-Light <onohsomtochukwubasil@gmail.com>
2023-09-23 19:56:25 +0100

    changed accesstoken from cookie to header

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -31,12 +31,16 @@ const handleLoginController = async (req, res) => {
       });
     }
 
-    res.cookie('token', token, {
-      maxAge: 24 * 60 * 60 * 1000 * 5,
-      httpOnly: true,
-    }); // 5 days for cookie age
+    // res.cookie('token', token, {
+    //   maxAge: 24 * 60 * 60 * 1000 * 5,
+    //   httpOnly: true,
+    // }); // 5 days for cookie age
 
-    return res.status(200).json({ message: 'Authentication successful', user });
+    return res.status(200).json({
+      message: 'Authentication successful',
+      user,
+      access_token: token,
+    });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
